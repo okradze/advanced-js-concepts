979a875c68166c9fccb170e2c48c429aef718297
Mirian Okradze <okradzemirian@gmail.com>
2020-09-12 14:44:34 +0400

    memory heap

---
+++ memoryHeap.js
@@ -0,0 +1,6 @@
+const number = 10 // allocate memory for number
+const str = 'some text' // allocate memory for a string
+const obj = { // allocate memory for an object and it's values
+  a: 1,
+  b: 2
+}
